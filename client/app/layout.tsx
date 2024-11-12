0082e936fd7ce001df896462765ad455a8768235
mtarek7900 <hamim7900@gmail.com>
2024-11-12 16:17:13 -0500

    Added Bootstrap and basic navbar to client

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,6 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          <nav className= "navbar bg-body-tertiary" data-bs-theme="dark">
+            <div className="container-fluid">
+              <span className="navbar-brand mb-0 h1">Echo Lounge</span>
+            </div>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
